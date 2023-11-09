15d7f2c7ea973c9fe9ad0dd447484117a3f9c4a7
Nourezzehi <nour.ezzhi.insat.ucar.tn>
2023-11-09 12:44:28 +0100

    base_model

---
+++ models/base_model.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""This is the BaseModel module"""
+import uuid
+from datetime import datetime
+time_format = "%Y-%m-%dT%H:%M:%S.%f"
+
+
+class BaseModel:
+    """ defines all common attributes/methods for other classes"""
+    def __init__(self, *args, **kwargs):
+        """constructor method"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+
+    def __str__(self):
+        """String representation of the object"""
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
+
+    def save(self):
+        """updates the public instance attribute updated_at with
+        the current datetime"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values of
+        __dict__ of the instance"""
+        new = self.__dict__
+        new["__class__"] = self.__class__.__name__
+        new["created_at"] = self.created_at.isoformat()
+        new["updated_at"] = self.updated_at.isoformat()
+        return new
